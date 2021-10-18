c1a7b2caf15f37e24c5258fd7aa1bfb6722b882a
hossein s. borhani <junguler@yahoo.com>
2021-10-18 16:36:21 +0330

    add resize keybinds, change inactive pane hsb

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,17 +3,17 @@ return {
   default_prog = {"/bin/zsh", "-l"},
   font_size = 11.0,
   line_height = 0.8, -- 0.9
-  bold_brightens_ansi_colors = false,
   check_for_updates = false,
   show_update_window = false,
   pane_focus_follows_mouse = true,
   window_close_confirmation = "NeverPrompt",
   disable_default_key_bindings = true,
   disable_default_mouse_bindings = true,
-  -- hide_tab_bar_if_only_one_tab = true,
+  hide_tab_bar_if_only_one_tab = true,
   default_cursor_style = "BlinkingBar", -- "SteadyBar"
   cursor_blink_rate = 600,
   color_scheme = "MaterialDarker", -- "SpaceGray Eighties"
+  bold_brightens_ansi_colors = false,
   window_padding = {
     left = 6,
     right = 6,
@@ -25,6 +25,10 @@ return {
       background = "#212121",
       },
     },
+  inactive_pane_hsb = {
+      saturation = 0.6,
+      brightness = 0.6,
+  },
   keys = {
     {key="v", mods="ALT",
       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
@@ -35,13 +39,19 @@ return {
     {key="w", mods="ALT",
      action=wezterm.action{CloseCurrentPane={confirm=false}}},
     {key="v", mods="CTRL", action="Paste"},
+    {key="LeftArrow", mods="ALT", action=wezterm.action{AdjustPaneSize={"Left", 27}}},
+    {key="DownArrow", mods="ALT", action=wezterm.action{AdjustPaneSize={"Down", 1}}},
+    {key="UpArrow", mods="ALT", action=wezterm.action{AdjustPaneSize={"Up", 1}}},
+    {key="RightArrow", mods="ALT", action=wezterm.action{AdjustPaneSize={"Right", 27}}},
   },
   font_rules = {
     {
-      italic = false,
-      bold = false,
-      intensity = "Normal",
-      font = wezterm.font("Cascadia Mono PL", {bold=false}),
+      -- italic = false,
+      -- bold = false,
+      -- intensity = "Normal", -- Medium
+      -- weight = "Regular",
+      -- font = wezterm.font("Cascadia Mono PL"),
+      font = wezterm.font("Cascadia Mono PL", {weight="Regular", italic=false, bold=false}) -- Medium
       -- font = wezterm.font("Hack"),
     }
   },
